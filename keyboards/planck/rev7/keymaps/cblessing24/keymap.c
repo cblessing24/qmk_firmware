4461a4cb3fd489b5150124af5e4a4c4ae3d1c3bc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2023-09-10 23:01:15 +0200

    Enable home row mods

--- keyboards/planck/rev7/keymaps/cblessing24/keymap.c
+++ keyboards/planck/rev7/keymaps/cblessing24/keymap.c
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_RSFT, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,  KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,    KC_Y,   KC_U,               KC_I,               KC_O,               KC_P,                  KC_BSPC,
+    KC_ESC,  MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G,    KC_H,   MT(MOD_RSFT, KC_J), MT(MOD_RCTL, KC_K), MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
+    KC_LSFT, KC_Z,               KC_X,               KC_C,               KC_V,               KC_B,    KC_N,   KC_M,               KC_COMM,            KC_DOT,             KC_SLSH,               KC_ENT ,
+    BACKLIT, KC_LCTL,            KC_LALT,            KC_LGUI,            LOWER,              KC_RSFT, KC_SPC, RAISE,              KC_LEFT,            KC_DOWN,            KC_UP,                 KC_RGHT
 ),
 
 /* Colemak
